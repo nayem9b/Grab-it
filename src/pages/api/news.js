3b60617f7eb758bb50ecdcac1519fa8046107017
nayem9b <razwanniam1@gmail.com>
2023-07-21 21:58:57 +0600

    database connection successful

--- src/pages/api/news.js
+++ src/pages/api/news.js
@@ -1,5 +1,26 @@
-const handler = (req, res) => {
-  res.send({ message: "server created" });
-};
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://nayem:SqvH0NNaQWRXeEJl@cluster0.dafmrk2.mongodb.net/?retryWrites=true&w=majority";
 
-export default handler;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    console.log("database connected");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+export default run;
